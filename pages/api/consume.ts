7d06af6ff4870f8693fd9f66a7432c3b29bde77e
PredragWebDev <pedjolinodev@gamil.com>
2023-08-18 23:03:56 +0900

    modify

--- pages/api/consume.ts
+++ pages/api/consume.ts
@@ -20,77 +20,115 @@ export default async function handler(
   const targetIndex = req.headers['x-index-name'] as string;
   const pineconeEnvironment = req.headers['x-environment'];
 
-  const pinecone = await initPinecone(
-    pineconeApiKey as string,
-    pineconeEnvironment as string,
-  );
-
-  if (pinecone == null) {
-    res.status(500).json({ error: "Invalid pinecone" });    
-    return;
-  }
-
-  const { namespaceName, chunkSize, overlapSize } = req.query;
-
-  let currentPath = process.cwd();
-  console.log('current path>>>', currentPath);
-  currentPath += '\\namespace';
-
-  if (!fs.existsSync(currentPath)) {
-
-    fs.mkdirSync(currentPath);
-  }
-  currentPath += '\\' + namespaceName;
-
-  if (fs.existsSync(currentPath)) {
-    const filesToDelete = fs
-        .readdirSync(currentPath)
-        .filter(
-          (file) =>
-            file.endsWith('.pdf') ||
-            file.endsWith('.docx') ||
-            file.endsWith('.txt') ||
-            file.endsWith('.csv'),
-        );
-      filesToDelete.forEach((file) => {
-        fs.unlinkSync(`${currentPath}/${file}`);
-      });
-  } else {
-    fs.mkdirSync(currentPath);
-  }
-  
-  
   try {
 
-    fs.readdir(filePath, async (error, uploadedFiles) => {
-      if (error) {
-        console.error('Error reading directory:', error);
-        return;
-      }
-
-      console.log('uploaded files>>>', uploadedFiles);
-
-      for (const file of uploadedFiles) {
-        // Load PDF, DOCS, TXT, CSV files from the specified directory
-        const directoryLoader = new TextLoader(filePath + "\\" + file);
-        const filecontent = await directoryLoader.load();
-        console.log('filecontent>>>>', filecontent);
-
-        // const modifiedFileContent = filecontent.replace('"', "'");
-
-        const docs = await textSplitter.splitDocuments(filecontent);
-
-        fs.writeFileSync(currentPath + '\\' + file, JSON.stringify(docs));
-
-        await PineconeStore.fromDocuments(docs, embeddings, {
-          pineconeIndex: index,
-          namespace: namespaceName as string,
-          textKey: 'text',
+    const pinecone = await initPinecone(
+      pineconeApiKey as string,
+      pineconeEnvironment as string,
+    );
+  
+    console.log('pinecone okay?')
+  
+    if (pinecone == null) {
+      res.status(500).json({ error: "Invalid pinecone" });    
+      return;
+    }
+  
+    const { namespaceName, chunkSize, overlapSize } = req.query;
+  
+    let currentPath = process.cwd();
+    console.log('current path>>>', currentPath);
+    currentPath += '\\namespace';
+  
+    if (!fs.existsSync(currentPath)) {
+  
+      fs.mkdirSync(currentPath);
+    }
+    currentPath += '\\' + namespaceName;
+  
+    if (fs.existsSync(currentPath)) {
+      const filesToDelete = fs
+          .readdirSync(currentPath)
+          .filter(
+            (file) =>
+              file.endsWith('.pdf') ||
+              file.endsWith('.docx') ||
+              file.endsWith('.txt') ||
+              file.endsWith('.csv'),
+          );
+        filesToDelete.forEach((file) => {
+          fs.unlinkSync(`${currentPath}/${file}`);
         });
+    } else {
+      fs.mkdirSync(currentPath);
+    }
+    
+    
+    try {
+  
+      fs.readdir(filePath, async (error, uploadedFiles) => {
+        if (error) {
+          console.error('Error reading directory:', error);
+          return;
+        }
+  
+        console.log('uploaded files>>>', uploadedFiles);
+  
+        for (const file of uploadedFiles) {
+          // Load PDF, DOCS, TXT, CSV files from the specified directory
+          const directoryLoader = new TextLoader(filePath + "\\" + file);
+          const filecontent = await directoryLoader.load();
+          console.log('filecontent>>>>', filecontent);
+  
+          // const modifiedFileContent = filecontent.replace('"', "'");
+  
+          const docs = await textSplitter.splitDocuments(filecontent);
+  
+          fs.writeFileSync(currentPath + '\\' + file, JSON.stringify(docs));
+  
+          await PineconeStore.fromDocuments(docs, embeddings, {
+            pineconeIndex: index,
+            namespace: namespaceName as string,
+            textKey: 'text',
+          });
+          
+        }
+        // Delete the PDF, DOCX, TXT, CSV files
         
-      }
+        const filesToDelete = fs
+          .readdirSync(filePath)
+          .filter(
+            (file) =>
+              file.endsWith('.pdf') ||
+              file.endsWith('.docx') ||
+              file.endsWith('.txt') ||
+              file.endsWith('.csv'),
+          );
+        filesToDelete.forEach((file) => {
+          fs.unlinkSync(`${filePath}/${file}`);
+        });
+      })
+      // Split the PDF documents into smaller chunks
+      // const textSplitter = new RecursiveCharacterTextSplitter({
+      //   chunkSize: Number(chunkSize),
+      //   chunkOverlap: Number(overlapSize),
+      // });
+  
+      // // OpenAI embeddings for the document chunks
+      // const embeddings = new OpenAIEmbeddings({
+      //   openAIApiKey: openAIapiKey as string,
+      // });
+  
+      // // Get the Pinecone index with the given name
+      // const index = pinecone.Index(targetIndex);
+  
+      // Store the document chunks in Pinecone with their embeddings
+  
+      res.status(200).json({ message: 'Data ingestion complete' });
+    } catch (error) {
+      console.log('error>>>>', error.message);
+  
       // Delete the PDF, DOCX, TXT, CSV files
-      
       const filesToDelete = fs
         .readdirSync(filePath)
         .filter(
@@ -103,50 +141,19 @@ export default async function handler(
       filesToDelete.forEach((file) => {
         fs.unlinkSync(`${filePath}/${file}`);
       });
-    })
-    // Split the PDF documents into smaller chunks
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: Number(chunkSize),
-      chunkOverlap: Number(overlapSize),
-    });
-
-    // OpenAI embeddings for the document chunks
-    const embeddings = new OpenAIEmbeddings({
-      openAIApiKey: openAIapiKey as string,
-    });
-
-    // Get the Pinecone index with the given name
-    const index = pinecone.Index(targetIndex);
-
-    // Store the document chunks in Pinecone with their embeddings
-
-    res.status(200).json({ message: 'Data ingestion complete' });
-  } catch (error) {
-    console.log('error>>>>', error.message);
-
-    // Delete the PDF, DOCX, TXT, CSV files
-    const filesToDelete = fs
-      .readdirSync(filePath)
-      .filter(
-        (file) =>
-          file.endsWith('.pdf') ||
-          file.endsWith('.docx') ||
-          file.endsWith('.txt') ||
-          file.endsWith('.csv'),
-      );
-    filesToDelete.forEach((file) => {
-      fs.unlinkSync(`${filePath}/${file}`);
-    });
-
-    if (error) {
-      if (error.message === 'Request failed with status code 401') {
-        res.status(500).json({ error: error.message + ". Please check Openai Api key." });
+  
+      if (error) {
+        if (error.message === 'Request failed with status code 401') {
+          res.status(500).json({ error: error.message + ". Please check Openai Api key." });
+        } else {
+          res.status(500).json({ error: error.message + ". Please check files again." });
+        }
+        
       } else {
-        res.status(500).json({ error: error.message + ". Please check files again." });
+        res.status(500).json({ error: "Failed ingestion. Please check files again." });
       }
-      
-    } else {
-      res.status(500).json({ error: "Failed ingestion. Please check files again." });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message + ". Please check pinecone." });
   }
 }
