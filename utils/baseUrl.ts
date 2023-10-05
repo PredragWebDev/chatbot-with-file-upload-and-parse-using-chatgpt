bb9196bb6a16bac3cd3222c162cf61ea9ff9a756
PredragWebDev <pedjolinodev@gmail.com>
2023-10-05 17:23:06 +0900

    cleaning code baseURL

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -3,7 +3,7 @@
 export const baseUrl = () => {
   const { hostname } = window.location;
   const isDomain = !/^(localhost|(\d{1,3}\.){3}\d{1,3})$/.test(hostname);
-
+  
   if (isDomain) {
     const domainName = window.location.protocol + "//" + window.location.hostname;
     return domainName;
